80b37b3b2a1e9170c4fd8141aa196171df6feb6a
Kaji Tetsushi <kajitetsushi@outlook.com>
2022-01-31 14:29:56 +0800

    Initial Commit

---
+++ hello-pong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-pong
+//
+//  Created by Kaji Tetsushi on 1/31/22.
+//  Copyright © 2022 Kaji Tetsushi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
